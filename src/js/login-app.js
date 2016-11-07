8416c3246cc4f5d30b402f39c8bffc76c4de3a07
Satit Rianpit <rianpit@gmail.com>
2016-11-07 09:35:40 +0700

    update color

--- src/js/login-app.js
+++ src/js/login-app.js
@@ -6,7 +6,7 @@ angular.module('app', [
   .config(($mdThemingProvider) => {
 
  $mdThemingProvider.theme('default')
-    .primaryPalette('indigo')
-    .accentPalette('pink');
+    .primaryPalette('blue-grey')
+    .accentPalette('orange');
 
   });
